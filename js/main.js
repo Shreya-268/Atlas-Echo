9ed1af14c23af99d27850d8d90128b00f9063dcf
Shreya <shreyaanchans7@gmail.com>
2025-08-02 12:33:26 +0530

    Add mouse position tracking to gallery items for enhanced interactivity

--- js/main.js
+++ js/main.js
@@ -152,3 +152,13 @@ function showNextInspireWord() {
 }
 
 setInterval(showNextInspireWord, 300);
+
+document.querySelectorAll('.gallery-item').forEach(item => {
+  item.addEventListener('mousemove', e => {
+    const rect = item.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    item.style.setProperty('--mouse-x', `${x}px`);
+    item.style.setProperty('--mouse-y', `${y}px`);
+  });
+});
